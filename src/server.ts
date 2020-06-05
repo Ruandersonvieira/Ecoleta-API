eca6c7023eb1e3fefd92884384a35bd9aa6dd2db
Ruanderson Vieira <ruanderson@accalia.io>
2020-06-05 07:23:16 -0300

    :sparkles: cors and dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import path from "path";
 import routes from "./routes";
+import cors from "cors";
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
-app.listen(3333);
+app.listen(process.env.APP_PORT);
